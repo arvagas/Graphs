982002707f6b530af0080951989f3e7437121bcb
Arvin Agas <arvagas@gmail.com>
2020-01-21 18:15:19 -0500

    modified add_vertex Graph method; earliest_ancestor working

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -17,7 +17,8 @@ class Graph:
         self.vertices = {}
 
     def add_vertex(self, vertex_id):
-        self.vertices[vertex_id] = set()
+        if vertex_id not in self.vertices:
+            self.vertices[vertex_id] = set()
 
     def add_edge(self, v1, v2):
         if v1 in self.vertices and v2 in self.vertices:
@@ -30,4 +31,36 @@ class Graph:
 
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # initialize variables
+    graph = Graph()    
+    stack = Stack()
+    early_origin = -1
+    max_path_len = 1
+
+    #  updating graph
+    for relation in ancestors:
+        # 0 is parent, 1 is child
+        graph.add_vertex(relation[0])
+        graph.add_vertex(relation[1])
+
+        # child will have a path towards parent
+        graph.add_edge(relation[1], relation[0])
+
+    stack.push([starting_node])
+    while stack.size() > 0:
+        path = stack.pop()
+        oldest = path[-1]
+
+        # handles path that are the same length, but takes the 'smaller' value
+        if len(path) >= max_path_len and oldest < early_origin \
+            or len(path) > max_path_len:
+            # ^ handles paths that don't continue off the bat
+            early_origin = oldest
+            max_path_len = len(path)
+        
+        for parent in graph.get_neighbors(oldest):
+            new_path = list(path)
+            new_path.append(parent)
+            stack.push(new_path)
+    
+    return early_origin

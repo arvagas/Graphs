b66c48ea1b1465969f78e683e1fa69861da1374b
Arvin Agas <arvagas@gmail.com>
2020-01-21 16:07:07 -0500

    swapped out queue for stack

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,18 +1,15 @@
-class Queue:
+class Stack():
     def __init__(self):
-        self.queue = []
-
-    def enqueue(self, value):
-        self.queue.append(value)
-
-    def dequeue(self):
+        self.stack = []
+    def push(self, value):
+        self.stack.append(value)
+    def pop(self):
         if self.size() > 0:
-            return self.queue.pop(0)
+            return self.stack.pop()
         else:
             return None
-
     def size(self):
-        return len(self.queue)
+        return len(self.stack)
 
 class Graph:
 
